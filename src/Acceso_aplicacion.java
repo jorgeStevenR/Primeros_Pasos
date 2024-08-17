038b6c47d81cfc63a2cb46da8f92e56cffd25571
jorgeStevenR <rodrigesj500@gmail.com>
2024-08-16 22:21:49 -0500

    Juego de azar

--- src/Acceso_aplicacion.java
+++ src/Acceso_aplicacion.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Acceso_aplicacion {
+import javax.swing.*;
+public class Acceso_aplicacion {
+    public static void main(String[] args) {
+
+        String clave ="Jorge";
+
+        String pass ="";
+
+        while(clave.equals(pass) == false){
+
+            pass = JOptionPane.showInputDialog("Ingrese su Clave");
+
+            if(clave.equals(pass) == false){
+
+                System.out.println("Clave incorrecta ");
+
+            }
+
+        }
+
+        System.out.println("Clave correcta");
+
+
+
+
+
+
+
+    }
 }
--- src/Adivinar_El_Numero.java
+++ src/Adivinar_El_Numero.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Adivinar_El_Numero {
+import java.util.*;
+public class Adivinar_El_Numero {
+    public static void main(String[] args) {
+
+        int aleatorio =(int)(Math.random()*100);
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numero = 0;
+
+        int intentos = 0;
+
+        while(numero!=aleatorio){
+
+            intentos++;
+
+            System.out.println("Introduzca un numero por favor");
+
+            numero = entrada.nextInt();
+
+            if(aleatorio<numero){
+
+                System.out.println("Dame un numero menor ");
+
+
+            } else if (aleatorio>numero) {
+                System.out.println("Dame un numero mayor");
+
+            }
+
+
+        }
+        System.out.println("Correcto");
+        System.out.println("Desifraste el numero en " + intentos + " intentos");
+
+    }
+
 }
