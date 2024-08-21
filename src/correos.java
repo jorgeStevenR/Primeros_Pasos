d3fdb382cef3f462c8cf68a81c29ead33b8db6e2
jorgeStevenR <rodrigesj500@gmail.com>
2024-08-21 10:30:26 -0500

    correos

---
+++ src/correos.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class correos {
+    public static void main(String[] args) {
+
+        int arroba = 0;
+
+        boolean punto =false;
+
+        String main = JOptionPane.showInputDialog("Introduce tu correo electronico");
+
+        for (int i = 0; i < main.length(); i++) {
+
+            if(main.charAt(i) == '@') {
+                arroba++;
+            }
+
+            if(main.charAt(i) == '.') {
+
+                punto=true;
+            }
+
+        }
+        if(arroba == 1 && punto == true ){
+            System.out.println("Tu correo es correcto");
+        }else {
+            System.out.println("Tu correo es incorrecto");
+        }
+
+    }
+}
---
+++ src/factorial.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+public class factorial {
+    public static void main(String[] args) {
+
+        Long resultado = 1L;
+        int numero =Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+
+        for (int i = numero; i > 0; i--){
+
+            resultado = resultado * i;
+
+
+        }
+
+        System.out.println("El factorial de " + numero + " es " + resultado);
+
+    }
+
+}
+
